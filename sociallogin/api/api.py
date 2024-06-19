3b2ce2123e4f72008aa2f366759e8ad18a09fe8c
alvarohc777 <aherrada@uninorte.edu.co>
2024-06-18 23:53:54 -0500

    update redirect urls to auth user routes

--- sociallogin/api/api.py
+++ sociallogin/api/api.py
@@ -8,8 +8,11 @@ FRONTEND_URL_BASE = settings.FRONTEND_URL
 
 
 @router.get("redirect/", auth=None)
-def login_redirect(request):
+def login_redirect(request, error=None):
+    print(request)
+    if error == "permission_denied":
+        return redirect(f"{FRONTEND_URL_BASE}/login?error=email_exists")
     if request.user.is_authenticated:
-        print(f"{FRONTEND_URL_BASE}/logout")
-        return redirect(f"{FRONTEND_URL_BASE}/logout")
+        print(f"{FRONTEND_URL_BASE}/user")
+        return redirect(f"{FRONTEND_URL_BASE}/user")
     return redirect(f"{FRONTEND_URL_BASE}/login")
